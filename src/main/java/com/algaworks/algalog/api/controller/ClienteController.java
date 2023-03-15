8f1b62247057cbc638d9e562954cca2d97e3f232
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2023-03-15 20:50:07 -0300

    feat: Validação com Bean Validation e ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,13 +42,13 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("{clienteId}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
-			@RequestBody Cliente cliente) {
+			@Valid @RequestBody Cliente cliente) {
 		
 		if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
